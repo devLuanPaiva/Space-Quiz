3e5ef4d8ee5f39737fcda4f2b78143b6870798a5
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-07-29 15:24:58 -0300

    feat: cria interface e constant de questões

---
+++ data/constants/Questions.ts
@@ -0,0 +1,244 @@
+import Question from "../interface/Question";
+
+const questions: Question[] = [
+  {
+    id: 1,
+    statement: "Qual é o planeta mais próximo do Sol?",
+    options: ["Terra", "Marte", "Vênus", "Mercúrio", "Júpiter"],
+    response: 3,
+  },
+  {
+    id: 2,
+    statement: "Qual é o maior planeta do Sistema Solar?",
+    options: ["Saturno", "Júpiter", "Urano", "Netuno", "Terra"],
+    response: 1,
+  },
+  {
+    id: 3,
+    statement: "Qual é o planeta conhecido como 'Planeta Vermelho'?",
+    options: ["Marte", "Vênus", "Júpiter", "Saturno", "Mercúrio"],
+    response: 0,
+  },
+  {
+    id: 4,
+    statement: "Qual planeta tem anéis proeminentes?",
+    options: ["Marte", "Saturno", "Vênus", "Mercúrio", "Urano"],
+    response: 1,
+  },
+  {
+    id: 5,
+    statement: "Qual é o menor planeta do Sistema Solar?",
+    options: ["Terra", "Marte", "Vênus", "Mercúrio", "Plutão"],
+    response: 3,
+  },
+  {
+    id: 6,
+    statement: "Qual planeta é conhecido por ter uma grande mancha vermelha?",
+    options: ["Marte", "Saturno", "Júpiter", "Vênus", "Netuno"],
+    response: 2,
+  },
+  {
+    id: 7,
+    statement: "Qual é a maior lua de Saturno?",
+    options: ["Europa", "Ganímedes", "Tritão", "Titã", "Encélado"],
+    response: 3,
+  },
+  {
+    id: 8,
+    statement: "Qual planeta é conhecido por sua coloração azul?",
+    options: ["Urano", "Netuno", "Saturno", "Júpiter", "Vênus"],
+    response: 1,
+  },
+  {
+    id: 9,
+    statement: "Qual é o nome da galáxia em que vivemos?",
+    options: ["Andrômeda", "Via Láctea", "Triângulo", "Sombrero", "Olho Negro"],
+    response: 1,
+  },
+  {
+    id: 10,
+    statement: "Qual planeta tem a maior rotação no Sistema Solar?",
+    options: ["Júpiter", "Saturno", "Marte", "Vênus", "Terra"],
+    response: 0,
+  },
+  {
+    id: 11,
+    statement: "Qual é o planeta mais quente do Sistema Solar?",
+    options: ["Marte", "Vênus", "Mercúrio", "Terra", "Júpiter"],
+    response: 1,
+  },
+  {
+    id: 12,
+    statement: "Qual é a maior lua de Júpiter?",
+    options: ["Io", "Europa", "Ganimedes", "Calisto", "Titã"],
+    response: 2,
+  },
+  {
+    id: 13,
+    statement: "Qual planeta tem ventos extremamente fortes e rápidos?",
+    options: ["Terra", "Saturno", "Urano", "Netuno", "Júpiter"],
+    response: 3,
+  },
+  {
+    id: 14,
+    statement: "Qual é o nome do maior vulcão do Sistema Solar?",
+    options: [
+      "Mauna Kea",
+      "Olimpo Mons",
+      "Monte Everest",
+      "Pico de Orizaba",
+      "K2",
+    ],
+    response: 1,
+  },
+  {
+    id: 15,
+    statement: "Qual planeta tem um dia mais longo que seu ano?",
+    options: ["Terra", "Marte", "Vênus", "Mercúrio", "Urano"],
+    response: 2,
+  },
+  {
+    id: 16,
+    statement: "Qual planeta possui uma lua chamada Tritão?",
+    options: ["Júpiter", "Saturno", "Urano", "Netuno", "Plutão"],
+    response: 3,
+  },
+  {
+    id: 17,
+    statement: "Qual planeta é conhecido como o 'Gigante de Gelo'?",
+    options: ["Saturno", "Júpiter", "Urano", "Netuno", "Plutão"],
+    response: 2,
+  },
+  {
+    id: 18,
+    statement: "Qual é o nome do cinturão de asteroides entre Marte e Júpiter?",
+    options: [
+      "Cinturão de Kuiper",
+      "Cinturão de Asteroides",
+      "Nuvem de Oort",
+      "Nuvem de Magalhães",
+      "Anel de Nebulosas",
+    ],
+    response: 1,
+  },
+  {
+    id: 19,
+    statement: "Qual é a estrela mais próxima da Terra?",
+    options: [
+      "Alpha Centauri",
+      "Sirius",
+      "Betelgeuse",
+      "Proxima Centauri",
+      "Sol",
+    ],
+    response: 4,
+  },
+  {
+    id: 20,
+    statement:
+      "Qual planeta é conhecido por suas grandes tempestades de areia?",
+    options: ["Terra", "Marte", "Vênus", "Júpiter", "Saturno"],
+    response: 1,
+  },
+  {
+    id: 21,
+    statement: "Qual planeta possui a maior quantidade de satélites naturais?",
+    options: ["Júpiter", "Saturno", "Urano", "Netuno", "Terra"],
+    response: 0,
+  },
+  {
+    id: 22,
+    statement: "Qual planeta tem a menor densidade do Sistema Solar?",
+    options: ["Mercúrio", "Terra", "Marte", "Saturno", "Urano"],
+    response: 3,
+  },
+  {
+    id: 23,
+    statement: "Qual é o nome do rover que pousou em Marte em 2021?",
+    options: [
+      "Curiosity",
+      "Spirit",
+      "Opportunity",
+      "Perseverance",
+      "Sojourner",
+    ],
+    response: 3,
+  },
+  {
+    id: 24,
+    statement: "Qual planeta tem a lua mais ativa geologicamente?",
+    options: ["Saturno", "Júpiter", "Urano", "Netuno", "Plutão"],
+    response: 1,
+  },
+  {
+    id: 25,
+    statement:
+      "Qual é o nome do primeiro satélite artificial lançado pela União Soviética em 1957?",
+    options: ["Apollo", "Sputnik", "Voyager", "Luna", "Cosmos"],
+    response: 1,
+  },
+  {
+    id: 26,
+    statement: "Qual é a principal composição da atmosfera de Marte?",
+    options: [
+      "Oxigênio",
+      "Hidrogênio",
+      "Metano",
+      "Dióxido de Carbono",
+      "Nitrogênio",
+    ],
+    response: 3,
+  },
+  {
+    id: 27,
+    statement:
+      "Qual é a unidade de medida da distância utilizada em astronomia?",
+    options: [
+      "Quilômetro",
+      "Milha",
+      "Unidade Astronômica",
+      "Ano-luz",
+      "Parsec",
+    ],
+    response: 3,
+  },
+  {
+    id: 28,
+    statement: "Qual é a fase da Lua quando ela está completamente iluminada?",
+    options: [
+      "Lua Nova",
+      "Quarto Crescente",
+      "Quarto Minguante",
+      "Lua Cheia",
+      "Lua Crescente",
+    ],
+    response: 3,
+  },
+  {
+    id: 29,
+    statement: "Qual planeta é conhecido por seus ventos supersônicos?",
+    options: ["Vênus", "Marte", "Saturno", "Netuno", "Urano"],
+    response: 3,
+  },
+  {
+    id: 30,
+    statement: "Qual é a maior lua de Netuno?",
+    options: ["Tritão", "Europa", "Calisto", "Io", "Titã"],
+    response: 0,
+  },
+  {
+    id: 31,
+    statement:
+      "Qual é o nome do buraco negro supermassivo no centro da Via Láctea?",
+    options: ["Sagittarius A*", "M87", "Cygnus X-1", "Ton 618", "NGC 1277"],
+    response: 0,
+  },
+  {
+    id: 32,
+    statement: "Qual planeta possui a Grande Mancha Escura?",
+    options: ["Júpiter", "Saturno", "Urano", "Netuno", "Vênus"],
+    response: 3,
+  },
+];
+
+export default questions;
