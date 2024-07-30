ca091c333487374d9b73ea5f05cf812e985fcf9e
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-07-30 10:55:37 -0300

    feat: implementa exibição dos componentes

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
 import Logo from "@/components/template/Logo";
 import Page from "@/components/template/Page";
 import Question from "@/components/quiz/Question";
-import questions from "@/data/constants/Questions";
 import { View } from "react-native";
+import useQuiz from "@/data/hooks/useQuiz";
+import Result from "@/components/quiz/Result";
 
 export default function Index() {
+    const { question, response, completed, punctuation, totalQuestions, restart } = useQuiz()
     return (
         <Page>
-            <View style={{gap: 40}}>
+            <View style={{ gap: 40 }}>
                 <Logo />
-                <Question question={questions[0]}/>
+                {completed ? (
+                    <Result
+                        punctuation={punctuation}
+                        amountQuestions={totalQuestions}
+                        restart={restart}
+                    />
+                ) : (
+                    <Question
+                        question={question}
+                        optionSelected={response}
+                    />
+                )}
             </View>
         </Page>
     )
